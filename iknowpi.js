815329c9622845293e6f71535a510d75e29e0d78
William Edwin Sharkey <williamsharkey@gmail.com>
2016-05-20 12:21:05 -0400

    colors and more

--- iknowpi.js
+++ iknowpi.js
@@ -3,20 +3,40 @@
  */
 
 var pi = "3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679";
-
+var htmlEl = document.querySelector('html');
 var audio = new window.AudioContext();
 var currEl = document.querySelector('#current');
 var pastEl = document.querySelector('#past');
+var scoreEl = document.querySelector('#score');
 
 var keycodes =  [65,    83,68,70,   71,72,74,       75,76,186];//home row
 var keycodes2 = [48,    49,50,51,   52,53,54,       55,56,57]; //nums
 var keycodes3 = [96,    97,98,99,   100,101,102,    103,104,105]; // numpad
+var spaceCode = 32;
 var keys = "asdfghjkl;";
 var digits = "0123456789";
 
 var root = 200;
 var currPlace = 0;
+var score = 0;
+var failed = false;
+
+function reset() {
+    score =0;
+    scoreEl.innerHTML = score;
+    pastEl.innerHTML = '';
+    currPlace = 0;
+    failed = false;
+    htmlEl.classList.remove("failed");
+
+}
+
 function play(e){
+    if (e.which == spaceCode) {
+        return reset();
+    }
+
+
     var index = keycodes.indexOf(e.which);
     var index2 = keycodes2.indexOf(e.which);
     var index3 = keycodes3.indexOf(e.which);
@@ -32,8 +52,15 @@ function play(e){
         var currClass;
         if (correct) {
             currClass = 'correct';
+            if (!failed){
+                score++;
+                scoreEl.innerHTML = score;
+            }
+
         } else {
+            failed = true;
             currClass = 'incorrect';
+            htmlEl.classList.add("failed");
         }
 
         pastEl.innerHTML += "<span class='"+currClass+"'>" +  pi[currPlace] + "</span>" ;
@@ -51,7 +78,7 @@ function play(e){
         //currEl.innerHTML = digits[index];
         //pastEl.innerHTML += keys[index];
     } else {
-        currEl.innerHTML = "";
+        //currEl.innerHTML = "";
         //createOscillator(root/4);
     }
 
