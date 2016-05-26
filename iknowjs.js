f58518f89d0d1dfff66313b89fc855f9d61fa0c6
WilliamSharkey <williamsharkey@gmail.com>
2016-05-25 22:05:56 -0400

    inital work for a plug-able 'story mode'. Lots of work to do yet on this, but the essential idea is there: a story mode is a self contained javascript object that modifies the current 'card' based on the current pi-digit(and possibly other state information). A simple example with words is shown.

--- iknowjs.js
+++ iknowjs.js
@@ -15,9 +15,45 @@
 
 var pi = '31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989';
 
+
+// this is an example of a pluggable story mode. This is the simplest example. it just adds the current number to el
+var storyModeCount = {
+    pushPiDigit : function() {
+        var name = 'Count';
+        return function(currentDigit, el) {
+            var storyEl = element('<story>' + name + ' ' + currentDigit + '</story>');
+            el.appendChild(storyEl);
+        };
+    }()
+};
+
+
+// here is a slightly less trivial story mode. it associates each digit of pi with a word.
+var storyModeEdwin = {
+    pushPiDigit : function() {
+        var name = 'Edwin';
+        var words =  ['zen', 'unicycle', 'eye', 'triangle', 'square', 'pentagon', 'hexagon', 'wonders', 'octopus', 'cat lives'];
+        return function(currentDigit, el) {
+            var storyEl = element('<story>' + words[currentDigit] + '</story>');
+            el.appendChild(storyEl);
+        };
+    }()
+};
+
+
+var storyModes = [storyModeEdwin, storyModeCount];
+
+
 // this is a shortcut so people don't have to type document.querySelector ewww
 function q(selector) {
     return document.querySelector(selector);
+}
+
+// shortcut to create a new element from a html string
+function element(html) {
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    return div.firstChild;
 }
 
 var htmlEl = q('html');
@@ -190,9 +226,13 @@ function play(numberPressed) {
                 htmlEl.classList.add("failed");
             }
 
-            pastEl.insertAdjacentHTML('beforeend', "<table class='" + currClass + " digit" + pi[currPlace] + "'>" + padHt + "</table>");
+
+            var newTableEl = element("<table class='" + currClass + " digit" + pi[currPlace] + "'>" + padHt + "</table>");
+            var selectedStoryMode = storyModes[0];
+            selectedStoryMode.pushPiDigit(pi[currPlace], newTableEl);
+            pastEl.appendChild(newTableEl);
             pastEl.scrollTop = 0;
-            currPlace++;
+            currPlace = currPlace + 1;
             blankCards = 0;
         }
     }
